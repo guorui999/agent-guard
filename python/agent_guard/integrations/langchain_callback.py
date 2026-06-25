c9dc59a3b6e6d5dbe84415e2d32d0e1d41dad663
guorui999 <512414548@qq.com>
2026-06-26 01:05:47 +0800

    chore: remove unused AgentFinish import in langchain_callback

--- python/agent_guard/integrations/langchain_callback.py
+++ python/agent_guard/integrations/langchain_callback.py
@@ -5,7 +5,7 @@ import json
 from typing import Any, Dict, List, Optional
 
 from langchain_core.callbacks import BaseCallbackHandler
-from langchain_core.agents import AgentAction, AgentFinish
+from langchain_core.agents import AgentAction
 
 from ..engine import PolicyEngine
 
